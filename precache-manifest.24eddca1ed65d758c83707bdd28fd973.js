d9e8504e6b86703a00b130026e6f2428d6f1b217
egealpay <egealpay1@gmail.com>
2020-05-05 19:18:18 +0300

    Updates

---
+++ precache-manifest.24eddca1ed65d758c83707bdd28fd973.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6230c6ff1817726f862877f1ccc44e14",
+    "url": "/be-auto/index.html"
+  },
+  {
+    "revision": "e81bebe448b18b2ec62b",
+    "url": "/be-auto/static/css/2.e5b99fdb.chunk.css"
+  },
+  {
+    "revision": "a805367276ce428d0e9f",
+    "url": "/be-auto/static/css/main.79141d91.chunk.css"
+  },
+  {
+    "revision": "e81bebe448b18b2ec62b",
+    "url": "/be-auto/static/js/2.2ebcec97.chunk.js"
+  },
+  {
+    "revision": "7909880966503a9a44db65c2f7cf17ee",
+    "url": "/be-auto/static/js/2.2ebcec97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a805367276ce428d0e9f",
+    "url": "/be-auto/static/js/main.b51e7d3f.chunk.js"
+  },
+  {
+    "revision": "5165e309218851a66e5d",
+    "url": "/be-auto/static/js/runtime-main.a882b6e3.js"
+  },
+  {
+    "revision": "438b6ef22e37c43347031e521a223490",
+    "url": "/be-auto/static/media/service.438b6ef2.png"
+  }
+]);
